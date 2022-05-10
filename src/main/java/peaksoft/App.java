e043076ac2136b1bb7ce723527070f46b211a051
Muhammed <muhammedtoichubai@gmail.com>
2022-05-10 06:52:54 -0600

    first commit

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -10,5 +10,30 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat one = (Cat) applicationContext.getBean("cat", Cat.class);
+        System.out.println("One - Cat\n[");
+        System.out.println("Name:" + one.getName());
+        System.out.println("Age: " + one.getAge());
+        System.out.println("Breed: "  +one.getBreed());
+        System.out.println("Color: " + one.getColor()+"\n]");
+
+        Cat two = (Cat) applicationContext.getBean("cat", Cat.class);
+        System.out.println("Two - Cat\n[");
+        System.out.println("Name:" + two.getName());
+        System.out.println("Age: " + two.getAge());
+        System.out.println("Breed: " + two.getBreed());
+        System.out.println("Color: " + two.getColor()+"\n]");
+
+        Boolean comparison = one.equals(two);
+        System.out.println("comparison = " + comparison);
+        System.out.println(one);
+        System.out.println(two);
+
+
     }
 }
--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -2,6 +2,7 @@ package peaksoft;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -11,4 +12,20 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
-}
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat("Tom", 7, "Persian", "Navy blue");
+        return cat;
+    }
+
+//    Cat getCat(){
+//        Cat cat = new Cat();
+//        cat.setName("Tom");
+//        cat.setAge(7);
+//        cat.setBreed("Persian");
+//        cat.setColor("Navy blue");
+//        return cat;
+    }
+
